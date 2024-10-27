b407e221c2c05119066dcdc6976ae8a2c8697620
Ital023 <contadoitalo911@gmail.com>
2024-10-26 23:38:06 -0300

    feat: User Controller

--- src/main/java/io/github/Ital023/MovieflixDesafioItal023/controllers/UserController.java
+++ src/main/java/io/github/Ital023/MovieflixDesafioItal023/controllers/UserController.java
@@ -1,5 +1,7 @@
 package io.github.Ital023.MovieflixDesafioItal023.controllers;
 
+import io.github.Ital023.MovieflixDesafioItal023.dto.UserDTO;
+import io.github.Ital023.MovieflixDesafioItal023.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -7,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.movieflix.dto.UserDTO;
-import com.devsuperior.movieflix.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
