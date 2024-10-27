e1d449a35713aff4086da998e569a0e674e99286
Ital023 <contadoitalo911@gmail.com>
2024-10-26 22:51:12 -0300

    feat: initial files

---
+++ src/main/java/io/github/Ital023/MovieflixDesafioItal023/MovieflixDesafioItal023Application.java
@@ -0,0 +1,13 @@
+package io.github.Ital023.MovieflixDesafioItal023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieflixDesafioItal023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieflixDesafioItal023Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/Ital023/MovieflixDesafioItal023/MovieflixDesafioItal023ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Ital023.MovieflixDesafioItal023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieflixDesafioItal023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
