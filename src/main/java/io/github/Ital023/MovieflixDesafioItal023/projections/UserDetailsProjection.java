cc7e356536c3c9c86a6844c165c1c6ecab3ae3ba
Ital023 <contadoitalo911@gmail.com>
2024-10-26 23:26:53 -0300

    feat: Security User config

---
+++ src/main/java/io/github/Ital023/MovieflixDesafioItal023/projections/UserDetailsProjection.java
@@ -0,0 +1,9 @@
+package io.github.Ital023.MovieflixDesafioItal023.projections;
+
+public interface UserDetailsProjection {
+
+	String getUsername();
+	String getPassword();
+	Long getRoleId();
+	String getAuthority();
+}
