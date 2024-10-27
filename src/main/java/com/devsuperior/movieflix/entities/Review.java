92f367cb43fc0f3b9d7a381b5b1386bdc4839a1e
Ital023 <contadoitalo911@gmail.com>
2024-10-27 20:15:08 -0300

    feat: Review entity, ManyToOne user

---
+++ src/main/java/com/devsuperior/movieflix/entities/Review.java
@@ -0,0 +1,52 @@
+package com.devsuperior.movieflix.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_review")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Review() {
+    }
+
+    public Review(Long id, String text, User user) {
+        this.id = id;
+        this.text = text;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
