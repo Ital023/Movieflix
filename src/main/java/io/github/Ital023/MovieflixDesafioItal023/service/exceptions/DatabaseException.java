5a5e2509080c0ffe9d878eaafee18062d22a2019
Ital023 <contadoitalo911@gmail.com>
2024-10-26 23:26:35 -0300

    feat: Exceptions Service

---
+++ src/main/java/io/github/Ital023/MovieflixDesafioItal023/service/exceptions/DatabaseException.java
@@ -0,0 +1,7 @@
+package io.github.Ital023.MovieflixDesafioItal023.service.exceptions;
+
+public class DatabaseException extends RuntimeException {
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+}
---
+++ src/main/java/io/github/Ital023/MovieflixDesafioItal023/service/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package io.github.Ital023.MovieflixDesafioItal023.service.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	public ResourceNotFoundException(String msg) {
+		super(msg);
+	}
+}
