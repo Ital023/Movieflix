514225af608cca0914540f90f1eba85433434175
Ital023 <contadoitalo911@gmail.com>
2024-10-28 21:06:43 -0300

    feat: get paged movie sorted by title

--- src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
+++ src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
@@ -5,7 +5,9 @@ import com.devsuperior.movieflix.dto.MovieGenreDTO;
 import com.devsuperior.movieflix.services.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,10 +31,12 @@ public class MovieController {
         return ResponseEntity.ok().body(dto);
     }
 
-
+    @PreAuthorize("hasAnyRole('VISITOR', 'MEMBER')")
     @GetMapping
     public ResponseEntity<Page<MovieDTO>> getPaged(Pageable pageable) {
-        Page<MovieDTO> dto = service.getTest(pageable);
+        PageRequest pageRequest =
+                PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by("title"));
+        Page<MovieDTO> dto = service.getPaged(pageRequest);
         return ResponseEntity.ok().body(dto);
     }
 
--- src/main/java/com/devsuperior/movieflix/services/MovieService.java
+++ src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -8,6 +8,7 @@ import com.devsuperior.movieflix.repositories.MovieRepository;
 import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
